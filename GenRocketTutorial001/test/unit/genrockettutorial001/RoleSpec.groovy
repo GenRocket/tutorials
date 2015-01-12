07d3bb23a969b03b0341ab8f7a9aa35f322edf41
Gregg Bolinger <hycel1@gmail.com>
2015-01-12 09:43:08 -0800

    add test folder

---
+++ GenRocketTutorial001/test/unit/genrockettutorial001/RoleSpec.groovy
@@ -0,0 +1,21 @@
+package genrockettutorial001
+
+import genrockettutorial001.security.Role
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Role)
+class RoleSpec extends Specification {
+
+  def setup() {
+  }
+
+  def cleanup() {
+  }
+
+  void "test something"() {
+  }
+}
