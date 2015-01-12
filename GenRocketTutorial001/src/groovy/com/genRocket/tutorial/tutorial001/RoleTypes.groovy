2cc4cc4502f665aabc0d09993a1d338bb795e41d
Gregg Bolinger <hycel1@gmail.com>
2015-01-12 14:38:02 -0800

    New Roles enum.

---
+++ GenRocketTutorial001/src/groovy/com/genRocket/tutorial/tutorial001/RoleTypes.groovy
@@ -0,0 +1,28 @@
+package com.genRocket.tutorial.tutorial001
+/**
+ * Created by htaylor on 1/12/15.
+ */
+public enum RoleTypes {
+  ROLE_USER('ROLE_USER'),
+  ROLE_ADMIN('ROLE_ADMIN'),
+  ROLE_ORG_ADMIN('ROLE_ORG_ADMIN'),
+  ROLE_DEPT_ADMIN('ROLE_DEPT_ADMIN')
+
+  RoleTypes(value) {
+    this.value = value
+  }
+
+  private final String value
+
+  String getKey() {
+    name()
+  }
+
+  String getValue() {
+    value
+  }
+
+  String toString() {
+    value
+  }
+}
