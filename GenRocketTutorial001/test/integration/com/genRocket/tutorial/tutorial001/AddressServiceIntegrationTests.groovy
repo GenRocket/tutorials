6e1faa555717f053eaeaac474862c46a8d004970
Hycel Taylor <hycel1@gmail.com>
2015-01-18 09:11:49 -0800

    New integration test.

---
+++ GenRocketTutorial001/test/integration/com/genRocket/tutorial/tutorial001/AddressServiceIntegrationTests.groovy
@@ -0,0 +1,37 @@
+package com.genRocket.tutorial.tutorial001
+
+import com.genRocket.tutorial.tutorial001.dto.LoaderDTO
+import com.genRocket.tutorial.tutorial001.security.User
+import com.genRocket.tutorial.tutorial001.testDataLoaders.AddressLoader
+
+/**
+ * Created by htaylor on 1/18/15.
+ */
+class AddressServiceIntegrationTests extends GroovyTestCase {
+  def addressService
+  def userTestDataService
+
+  protected void setUp() {
+    super.setUp()
+
+    //userTestDataService.loadData()
+  }
+
+  protected void tearDown() {
+    super.tearDown()
+  }
+
+  void testCreateDepartment() {
+    userTestDataService.loadData()
+
+    def addresses = (LoaderDTO[]) AddressLoader.load()
+    def node = addresses[0]
+    def address = ((Address) node.object)
+    def user = User.first()
+
+    addressService.create(user, address)
+
+    assertNotNull "address.id should not be null", address.id
+    assertNotNull "User should have an address", UserAddress.findByUserAndAddress(user, address)
+  }
+}
