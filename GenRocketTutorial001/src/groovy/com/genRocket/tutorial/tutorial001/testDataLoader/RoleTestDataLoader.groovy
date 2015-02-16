c3d94165d9923fde287f6aeeb3056a9ae928f692
Hycel Taylor <hycel1@gmail.com>
2015-02-16 07:20:30 -0800

    Scenario name change.

--- GenRocketTutorial001/src/groovy/com/genRocket/tutorial/tutorial001/testDataLoader/RoleTestDataLoader.groovy
+++ GenRocketTutorial001/src/groovy/com/genRocket/tutorial/tutorial001/testDataLoader/RoleTestDataLoader.groovy
@@ -7,8 +7,8 @@ import com.genRocket.tutorial.tutorial001.security.Role
  * Created by htaylor on 1/15/15.
  */
 class RoleTestDataLoader extends TestDataLoaderBase {
-  static SCENARIO = "com.genRocket.Roles.grs"
-  static SCENARIO_DOMAIN = 'com.genRocket.Roles.Role'
+  static SCENARIO = "com.genRocket.Role.grs"
+  static SCENARIO_DOMAIN = 'com.genRocket.Role.Role'
 
   static load() {
     def roles = runScenario(new ScenarioParams(SCENARIO, SCENARIO_DOMAIN))
